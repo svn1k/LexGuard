a5d929f99d2d3f4122c902e7f5d5efcb7247787a
svn1k <svnik2015@gmail.com>
2026-04-05 20:00:47 +0300

    123

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,8 @@ from app import _init_og, _ping
 def post_fork(server, worker):
     threading.Thread(target=_init_og, daemon=True).start()
     threading.Thread(target=_ping, daemon=True).start()
+
+# Увеличиваем таймаут чтобы воркер не убивался пока OG грузится
+timeout = 120
+worker_class = "sync"
+workers = 1
